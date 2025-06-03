f76ed303258dab6a7d81ec5995b10dc008622508
one-media-asia <onemediaasia@duck.com>
2025-06-03 23:01:50 +0800

    teserd

--- mycrm/settings.py
+++ mycrm/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # STATIC_URL = 'static/' testme
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = 'static/'
-# Default primary key field type
+# Default primary key field typejjj
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
